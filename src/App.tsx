71ccaa351b1992cc5176357202e6d8e3626189bb
joshu <joshuapetitma@yahoo.com>
2020-11-05 18:08:24 -0500

    adds theming and Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import React from "react";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import { red, grey } from "@material-ui/core/colors/";
+import Nav from "./navigation/Nav";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: red[400],
+    },
+    type: "dark",
+    background: {
+      default: "#222222",
+    },
+    secondary: {
+      main: grey[100],
+    },
+  },
+});
+
+class App extends Component {
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+        <Nav />
+      </ThemeProvider>
+    );
+  }
 }
 
 export default App;
