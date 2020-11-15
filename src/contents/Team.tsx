cb99c7e5b17ff1a352578ac313c6f8bd59d06502
joshu <joshuapetitma@yahoo.com>
2020-11-15 07:54:03 -0500

    adds spacing between team image section

--- src/contents/Team.tsx
+++ src/contents/Team.tsx
@@ -3,6 +3,7 @@ import { Component } from "react";
 import { Hidden, Grid, Box, Container, Typography } from "@material-ui/core";
 import Image from "material-ui-image";
 
+
 interface Person {
   name: string;
   imageURL: string;
@@ -15,22 +16,24 @@ const persons: Person[] = [
     name: "First Last",
     imageURL: "placeholders/person.png",
     shortText: "text placeholder string string string",
-    longText: "text placeholder text placeholder text placeholder text placeholder text placeholder",
+    longText:
+      "text placeholder text placeholder text placeholder text placeholder text placeholder",
   },
   {
     name: "2 First Last",
     imageURL: "placeholders/person.png",
     shortText: "2 text placeholder string string string",
-    longText: "2 text placeholder text placeholder text placeholder text placeholder text placeholder",
+    longText:
+      "2 text placeholder text placeholder text placeholder text placeholder text placeholder",
   },
   {
     name: "3 First Last",
     imageURL: "placeholders/person.png",
     shortText: "3 text placeholder string string string",
-    longText: "3 text placeholder text placeholder text placeholder text placeholder text placeholder",
-  }
-]
-
+    longText:
+      "3 text placeholder text placeholder text placeholder text placeholder text placeholder",
+  },
+];
 
 export default class Team extends Component {
   render() {
@@ -39,38 +42,38 @@ export default class Team extends Component {
         <Typography color="primary" variant="h2" align="center">
           Meet The Team
         </Typography>
-        <Hidden  mdDown>
-          <Grid container justify="center" spacing={1} direction="row">
-            {persons.map( (x) => 
-            <Grid item xs={1}>
-              <Image src={x.imageURL} />
-            </Grid>
-            )}
+        <Hidden mdDown>
+          <Grid container justify="center" spacing={10} direction="row">
+            {persons.map((x) => (
+              <Grid 
+                item>
+                <img src={x.imageURL} />
+              </Grid>
+            ))}
           </Grid>
         </Hidden>
 
         <Container maxWidth="lg">
-        <Grid container justify="center" >
-          {persons.map((person) => 
-            // nthchild(odd) column-reverse
-            <Grid container spacing={1} direction="row">
-              <Grid item xs={12} md={3} spacing={1}>
-                {// Since the vertical images look so squished
-                  // we can create special crops and add a
-                  // .pt extension
-                 }
-               <Image src={person.imageURL+ ".pt"}/> 
+          <Grid style={{marginTop: '2em'}}container justify="center">
+            {persons.map((person) => (
+              // nthchild(odd) column-reverse
+              <Grid style={{marginTop: '1em'}} container spacing={1} direction="row">
+                <Grid item xs={12} md={3} spacing={1}>
+                  {
+                    // Since the vertical images look so squished
+                    // we can create special crops and add a
+                    // .pt extension
+                  }
+                  <Image src={person.imageURL + ".pt"} />
+                </Grid>
+                <Grid item xs={12} md={8} spacing={1}>
+                  <p>{person.name}</p>
+                  <p>{person.longText}</p>
+                </Grid>
               </Grid>
-              <Grid item xs={12} md={8} spacing={1}>
-                <p>{person.name}</p>
-                <p>{person.longText}</p>
-              </Grid>
-            </Grid>
-          )
-          }
-        </Grid>
-        </Container>  
-
+            ))}
+          </Grid>
+        </Container>
       </Container>
     );
   }
